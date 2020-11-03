44e38c586044aa4c6243e07400ba39b4a6013578
derek duan <derek@Derek-Mbp>
2020-11-03 14:17:29 +0800

    update 1103 3rd

---
+++ part3/13_文件/hm_08_计算器.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#coding:utf-8
+
+"""
+
+@author:derek
+@contract:derek_duan@sina.com
+@file: hm_08_计算器.py
+@time: 2020-11-03 11:24
+
+"""
+
+
+text = input("请输入算式：")
+print(eval(text))

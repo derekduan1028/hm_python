01e207386d8888ded676c52d4523d3c05fc84c64
derek duan <derek@Derek-Mbp>
2020-11-26 15:36:10 +0800

    update 1126 2nd

--- temp/test_for循环与列表推导对比.py
+++ temp/test_for循环与列表推导对比.py
@@ -9,3 +9,14 @@
 @time: 11/19/20 3:40 PM
 
 """
+
+import timeit
+fooo = """
+sum = []
+for i in range(1000):
+    sum.append(i)
+"""
+
+
+print(timeit.timeit(stmt="[i for i in range(1000)]", number=100000))
+print(timeit.timeit(stmt=fooo, number=100000))
